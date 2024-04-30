1f4656f6aac7b7620b4a3c99dc8d175bbff603af
romi9035 <romilgvit@gmail.com>
2024-04-30 09:47:41 +0000

    first commit

---
+++ webapp/index.js
@@ -0,0 +1,21 @@
+/* sap.ui.define([
+  "sap/m/Button",
+    "sap/m/MessageToast"
+], (Button, MessageToast) => {
+    "use strict";
+
+    new Button({
+        text: "Ready...",
+        press() {
+            MessageToast.show("Hello World!");
+        }
+    }).placeAt("content");
+
+});*/
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+], function(XMLView) {
+    'use strict';
+    
+    
+});
